9768323b9702d77c30cfbd086475136210620b0d
Splamy <splamyn@gmail.com>
2020-01-21 02:28:34 +0100

    yeah namespaces are nice

--- RamsesBeatsaberPlugin/Plugin.cs
+++ RamsesBeatsaberPlugin/Plugin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Newtonsoft.Json;
 
-namespace Ramses
+namespace RamsesBeatsaberPlugin
 {
 	public class RamsesPlugin : IBeatSaberPlugin
 	{
